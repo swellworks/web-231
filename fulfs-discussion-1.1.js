201b16658aead37267cf95d62b8242d2a3057824
Swellworks <perry@swellworks.com>
2020-10-20 23:04:57 -0500

    New js file for 1.1

---
+++ fulfs-discussion-1.1.js
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title: Discussion 1.1
+; Author: Perry Fulfs
+; Date: 20 October 2020
+; Modified By: Perry Fulfs
+; Description: This program demonstrates 
+; finding and fixing errors in a classmates program
+;===========================================
+*/ 
+
+// start program
+
+/* 
+
+    Expected output: 
+    Hello Everyone the 231 class has started! 
+    How will I do in the class? 
+    I hope to get passing grade.
+
+*/
+
+// variable 
+
+var message = "Hello Everyone" "the 231 class has started! \n";
+
+var otherWord = "How will I do in the class? \n"
+
+var myGrades = "I hope to a get passing grade.";
+
+// // output
+console.log(message + otherWord + myGrade);
+
+
+// end program
