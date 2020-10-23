588df7baf7f42da0c9df83f983c30de24ac1a17c
Swellworks <perry@swellworks.com>
2020-10-22 19:49:25 -0500

    new hello world file added

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,20 @@
+/*
+============================================
+; Title: Exercise 1.3
+; Author: Perry Fulfs
+; Date: 22 October 2020
+; Modified By: Perry Fulfs
+; Description: hello-world.js
+;===========================================
+*/ 
+
+// start program
+
+/* 
+    Expected output: 
+    Hello World, my name is Perry Fulfs
+*/
+
 console.log("Hello World, my name is Perry Fulfs");
+
+// end program
