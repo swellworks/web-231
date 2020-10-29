69f9aead9bccf4a7c08736c5c01a0fe94a88fac2
perry fulfs <swellworks@Macintosh-2.local>
2020-10-28 23:09:08 -0500

    header and test-header file commit

---
+++ week-2/fulfs-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  fulfs-header-test.js
+; Author: Professor Krasso
+; Date:   28 October 2020
+; Modified By: Perry Fulfs
+; Description: Displays a formatted header-test
+;===========================================
+*/
+
+const header = require('../fulfs-header.js');
+
+console.log(header.display("Perry", "Fulfs", "Exercise 2.2"));
