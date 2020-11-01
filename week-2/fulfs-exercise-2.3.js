2894924f38e009e2241c53d41d639462be9c8659
perry fulfs <swellworks@Macintosh-2.local>
2020-10-31 22:40:06 -0500

    commit asked to open folder

---
+++ week-2/fulfs-exercise-2.3.js
@@ -0,0 +1,48 @@
+/*
+============================================
+; Title:  fulfs-exercise-2.3.js
+; Author: Professor Krasso
+; Date:   30 October 2020
+; Modified By: Perry Fulfs
+; Description: function properties
+;===========================================
+*/
+
+// start program
+
+const header = require('../fulfs-header.js'); // added file from root directory
+
+console.log(header.display("Perry", "Fulfs", "Exercise 2.3")); // updated content with actual name & assignment name
+
+/*
+  Expected output:
+
+  FirstName LastName
+  Exercise 2.3
+  Today's Date
+
+  Hello FirstName LastName!
+
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+*/
+
+// add function properties here...
+firstName.perry = "Perry"; // Create a function property name with my real first name.
+lastName.fulfs = "Fulfs"; // Create a function property name with my real last name.
+
+//function
+function firstName() {
+  return firstName.perry; // Returns the firstName property from the function above.
+}
+
+function lastName() {
+  return lastName.fulfs; // Returns the lastName property from the function above.
+}
+
+//output
+console.log('\n');
+console.log("Hello " + firstName() + " " + lastName() + "!" ); // Calling the properties referencing the functions.
+// Concatenate the properties together by using the + sign.
+
+// end program
---
+++ week-2/fulfs-header-test.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  fulfs-header-test.js
+; Author: Professor Krasso
+; Date:   28 October 2020
+; Modified By: Perry Fulfs
+; Description: Displays a formatted header-test
+;===========================================
+*/
+
+const header = require('../fulfs-header.js'); // added file from root directory
+
+console.log(header.display("Perry", "Fulfs", "Exercise 2.2")); // updated content with actual name & assignment name
