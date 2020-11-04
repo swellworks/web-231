db24686d82af129de3fa5892de3aa82070c51f95
perry fulfs <swellworks@Macintosh-2.local>
2020-11-03 23:03:39 -0600

    discussion-3.1 with 2 errors

---
+++ week-3/fulfs-discussion-3.1.js
@@ -0,0 +1,26 @@
+/*
+============================================
+; Title:  fulfs-discussion-3.1.js
+; Author: Professor Krasso
+; Date:   3 November 2020
+; Modified By: Perry Fulfs
+; Description: Create a simple JavaScript program using either a
+; for, while, if, or switch statement with at least 2 errors
+===========================================
+*/
+
+// const header = require('../fulfs-header.js'); // added file from root directory
+
+// console.log(header.display("Perry", "Fulfs", "Discussion 3.1")); // updated content with actual name & assignment name
+
+// start program
+
+var n = 1;
+while(n = 8) { // Create loop that will continue to run as long as the variable n is less than or equal to 8.
+  n+;
+}
+
+// expected outcome - The number is 9.
+console.log("The number is " + n + ".");
+
+// end program
