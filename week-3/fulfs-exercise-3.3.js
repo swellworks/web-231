b1c200c86c20c0f513dbce892b5d84708e92a73b
perry fulfs <swellworks@Macintosh-2.local>
2020-11-07 20:31:58 -0600

    edited comments to exclude previous code

--- week-3/fulfs-exercise-3.3.js
+++ week-3/fulfs-exercise-3.3.js
@@ -30,18 +30,6 @@ console.log(header.display("Perry", "Fulfs", "Exercise 3.3")); //  content with 
 
 let eventKeyCode = 13
 
-/* if (eventKeyCode === 13) {
-  console.log('The enter key was pressed.')
-} else if (eventKeyCode === 16) {
-  console.log('The shift key was pressed.')
-} else if (eventKeyCode === 32) {
-  console.log('The spacebar key was pressed.')
-} else if (eventKeyCode === 8) {
-  console.log('The backspace / delete key was pressed.')
-} else {
-  console.log('Unrecognized key.')
-} */
-
 switch (eventKeyCode) { // switch statement eventKeyCode to execute code
   case 13: // start if 13 === The enter key was pressed
     console.log('The enter key was pressed');
