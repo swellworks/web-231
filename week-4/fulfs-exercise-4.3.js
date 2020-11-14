d982c2335dbd2545fb20666c7de5af01686c2249
perry fulfs <swellworks@Macintosh-2.local>
2020-11-14 15:03:10 -0600

    commit w/ comments final test

--- week-4/fulfs-exercise-4.3.js
+++ week-4/fulfs-exercise-4.3.js
@@ -37,34 +37,34 @@ const header = require('../fulfs-header.js'); // added file from root directory
 
 console.log(header.display("Perry", "Fulfs", "Exercise 4.3")); //  content with actual name & assignment name
 
-console.log("\n") // adding a space between header & rest of code
+console.log("") // adding a space between header & rest of code
 
 // ===========================================
 
-var vehicles = ['Car', 'Truck', 'Motorcycle', 'Airplane', 'Bus']
+var vehicles = ['Car', 'Truck', 'Motorcycle', 'Airplane', 'Bus'] // Variable string of 5 vehicles.
 
 // function
-function getValue(arr, val) {
-  for (var k = 0; k < arr.length; k++) {
-    if (arr[k] === val)
+function getValue(arr, val) { // Parameterized array and value to filter.
+  for (var k = 0; k < arr.length; k++) { // Iterate over the array.
+    if (arr[k] === val) // Use if statement to test the iterated element on the value.
       console.log(arr[k]);
   }
 }
 
 // output
 console.log("-- DISPLAYING ARRAY ITEMS --");
-  for (var x = 0; x < vehicles.length; x++){
+  for (var x = 0; x < vehicles.length; x++){ // Iterate over the initial array to output all 5 of the vehicles.
     console.log(vehicles[x]);
   }
 
 // new line
-console.log("")
+console.log("") // Empty string value to create space.
 
 console.log("-- SELECTED VALUE --");
-getValue(vehicles, 'Motorcycle');
+getValue(vehicles, 'Motorcycle'); // Pass in the value of vehicles and specify a value to find.
 
 // new line
-console.log("")
+console.log("") // Empty string value to create space.
 
 console.log("-- SELECTED VALUE --");
-getValue(vehicles, 'Bus');
+getValue(vehicles, 'Bus'); // Pass in the value of vehicles and specify a value to find.
